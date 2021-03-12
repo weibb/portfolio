9535ce890212949290cd67ac2339bdad2b9950e8
web <quiteripping@gmail.com>
2021-03-12 07:02:23 -0500

    Update for product design content

--- js/main.js
+++ js/main.js
@@ -1,35 +1,5 @@
-
-/** FRONT ANIMATION **/
-$(window).ready( function move_box(){
-    const row1 = document.getElementById("row1");
-    const row3 = document.getElementById("row3");
-
-    if (row1.classList.contains("translator")) {
-        $( '#row1' ).removeClass( "translator" );
-    } else {
-        $( '#row1' ).addClass( "translator" );
-    }
-    if (row3.classList.contains("translator2")) {
-        $( '#row3' ).removeClass( "translator2" );
-    } else {
-        $( '#row3' ).addClass( "translator2" );
-    }
-});
-
 /** NAME TOGGLE **/
 $('.name').hover(function () {
     $('#name').toggle();
     $('#namehover').toggle();
-});
-
-
-/** TITLE COLOR CHANGE **/
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 80) {
-        $('.title-text').addClass('title_w');
-    } else {
-        $('.title-text').removeClass('title_w');
-    }
 });
